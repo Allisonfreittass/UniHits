9e300e3b033c56ea8d04857025947806900e4240
Allison <allison.freitas@alunos.unis.edu.br>
2022-06-25 17:31:53 -0300

    Update

---
+++ js/music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+     {
+         nome: "Bones",
+         artista: "Imagine Dragons",
+         img: "music-1",
+         src:"music-1"
+     },
+     {
+        nome: "As It Was",
+        artista: "Harry Styles",
+        img: "asitwas",
+        src:"img/asitwas"
+    },
+    {
+        nome: "Do I Wanna Know",
+        artista: "Artctic Monkeys",
+        img: "diwk",
+        src:"img/diwk"
+    },
+    {
+        nome: "Shut Up And Listen",
+        artista: "Nicholas Bonnin",
+        img: "sual",
+        src:"img/sual"
+    },
+]
